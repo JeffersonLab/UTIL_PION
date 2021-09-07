09407df3f9cf2fd052ea736113fb74c42a7aa286
Cdaq User <cdaq@jlab.org>
2021-09-06 23:30:32 -0400

    Changed DEF_cuts and Report files to ones in tmp folders. THese are working so should consider adding these to main body as well

--- scripts/replay/replay_production_coin.C
+++ scripts/replay/replay_production_coin.C
@@ -257,11 +257,11 @@ void replay_production_coin (Int_t RunNumber = 0, Int_t MaxEvent = 0) {
   // Define DEF-file+
   analyzer->SetOdefFile("UTIL_PION/config/DEF-files/coin_production.def");
   // Define cuts file
-  analyzer->SetCutFile("UTIL_PION/config/DEF-files/coin_production_cuts.def");  // optional
+  analyzer->SetCutFile("UTIL_PION/config/DEF-files/tmp/Online_Coin_Production_Cuts.def");  // optional
   // File to record accounting information for cuts
   analyzer->SetSummaryFile(Form("UTIL_PION/config/REPORT_OUTPUT/summary_production_%d_%d.report", RunNumber, MaxEvent)); // optional
   // Start the actual analysis.
   analyzer->Process(run);
   // Create report file from template	       
-  analyzer->PrintReport("UTIL_PION/config/TEMPLATES/COIN/coin_production.template", Form("UTIL_PION/REPORT_OUTPUT/Analysis/PionLT/Pion_replay_coin_production_%d_%d.report", RunNumber, MaxEvent)); // optional}
+  analyzer->PrintReport("UTIL_PION/config/TEMPLATES/tmp/Online_Coin_Production.template", Form("UTIL_PION/REPORT_OUTPUT/Analysis/PionLT/Pion_replay_coin_production_%d_%d.report", RunNumber, MaxEvent)); // optional}
 }
