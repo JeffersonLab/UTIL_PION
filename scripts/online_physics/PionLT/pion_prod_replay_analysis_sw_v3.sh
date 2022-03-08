369aad4f18d801cc2207f1d7ba0950264b2b4e4f
sjdkay <sjdkay@jlab.org>
2022-03-08 17:40:04 -0500

    Added in the /u/ back to the paths. For some reason, it still seems to need this and crashes without it being included. I also added a loop that is needed to run on some farm nodes. Not all farm nodes are called *farm*, some are *qcd* so we need to set paths for these too

--- scripts/online_physics/PionLT/pion_prod_replay_analysis_sw_v3.sh
+++ scripts/online_physics/PionLT/pion_prod_replay_analysis_sw_v3.sh
@@ -34,6 +34,8 @@ if [[ ${HOSTNAME} = *"cdaq"* ]]; then
     PATHFILE_INFO=`python3 /home/cdaq/pionLT-2021/hallc_replay_lt/UTIL_PION/bin/python/ltsep/scripts/getPathDict.py $PWD` # The output of this python script is just a comma separated string
 elif [[ "${HOSTNAME}" = *"farm"* ]]; then
     PATHFILE_INFO=`python3 /u/home/${USER}/.local/lib/python3.4/site-packages/ltsep/scripts/getPathDict.py $PWD` # The output of this python script is just a comma separated string
+elif [[ "${HOSTNAME}" = *"qcd"* ]]; then
+    PATHFILE_INFO=`python3 /u/home/${USER}/.local/lib/python3.4/site-packages/ltsep/scripts/getPathDict.py $PWD` # The output of this python script is just a comma separated string
 fi
 
 # Split the string we get to individual variables, easier for printing and use later
