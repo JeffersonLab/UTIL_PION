037b9f898d21fee8ec7889caa1e9f998bfd7edb6
heinricn <heinricn@uregina.ca>
2024-04-03 13:49:00 -0600

    changed file path for 9.2 data to reflect that we have 2 sets of lumi scans

--- scripts/luminosity/src/PionLT/data_path.py
+++ scripts/luminosity/src/PionLT/data_path.py
@@ -19,21 +19,38 @@ def get_file(inp_name,SCRIPTPATH):
 
     # Depending on input, the corresponding data setting csv data will be grabbed
     if "9-2" in inp_name:
-        if "LH2" in inp_name.upper():
-            target = "LH2"
-            inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/lumi_data_LH2.csv"
-            out_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/yield_data_LH2.csv"
-            print("\nGrabbing input...\n\n%s" % str(inp_f))
-        if "LD2" in inp_name.upper():
-            target = "LD2"
-            inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/lumi_data_LD2.csv"
-            out_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/yield_data_LD2.csv"
-            print("\nGrabbing input...\n\n%s" % str(inp_f))
-        if "C" in inp_name.upper():
-            target = "carbon"
-            inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/lumi_data_Carbon.csv"
-            out_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/yield_data_Carbon.csv"
-            print("\nGrabbing input...\n\n%s" % str(inp_f))
+        if "pt1" in inp_name.lower():
+            if "LH2" in inp_name.upper():
+                target = "LH2"
+                inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt1/lumi_data_LH2.csv"
+                out_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt1/yield_data_LH2.csv"
+                print("\nGrabbing input...\n\n%s" % str(inp_f))
+            if "LD2" in inp_name.upper():
+                target = "LD2"
+                inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt1/lumi_data_LD2.csv"
+                out_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt1/yield_data_LD2.csv"
+                print("\nGrabbing input...\n\n%s" % str(inp_f))
+            if "C" in inp_name.upper():
+                target = "carbon"
+                inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt1/lumi_data_Carbon.csv"
+                out_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt1/yield_data_Carbon.csv"
+                print("\nGrabbing input...\n\n%s" % str(inp_f))
+        if "pt2" in inp_name.lower():
+            if "LH2" in inp_name.upper():
+                target = "LH2"
+                inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt2/lumi_data_LH2.csv"
+                out_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt2/yield_data_LH2.csv"
+                print("\nGrabbing input...\n\n%s" % str(inp_f))
+            if "LD2" in inp_name.upper():
+                target = "LD2"
+                inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt2/lumi_data_LD2.csv"
+                out_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt2/yield_data_LD2.csv"
+                print("\nGrabbing input...\n\n%s" % str(inp_f))
+            if "C" in inp_name.upper():
+                target = "carbon"
+                inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt2/lumi_data_Carbon.csv"
+                out_f = SCRIPTPATH+"/luminosity/OUTPUTS/Lumi9-2/pt2/yield_data_Carbon.csv"
+                print("\nGrabbing input...\n\n%s" % str(inp_f))
     elif "6-4" in inp_name:
         if "SHMS" in inp_name.upper():
             if "LH2" in inp_name.upper():
