71df6f41379a53f3b68953b7e63f7582c0e3dfec
heinricn <heinricn@uregina.ca>
2024-04-12 09:24:47 -0600

    added edtm time plot into plots

--- scripts/luminosity/src/lumiyield.py
+++ scripts/luminosity/src/lumiyield.py
@@ -298,15 +298,22 @@ def pid_cuts():
     f = plt.figure(figsize=(11.69,8.27))
     f.suptitle("Run %s" % runNum)
 
+    ax = f.add_subplot(331)
+    ax.hist(tree["T_coin_pEDTM_tdcTimeRaw"],bins=c.bins(tree["T_coin_pEDTM_tdcTimeRaw"],10000,-1000,9000),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
+    ax.hist(c.add_cut(tree["T_coin_pEDTM_tdcTimeRaw"],"c_edtm"),bins=c.bins(tree["T_coin_pEDTM_tdcTimeRaw"],10000,-1000,9000),label='cut',histtype='step',alpha=0.5, stacked=True, fill=True)
+    plt.yscale('log')
+    plt.xlabel('pEDTM_tdcTimeRaw')
+    plt.ylabel('Count')
+
     if (not HMS_PS == None) or (not COIN_PS == None):
-        ax = f.add_subplot(231)
+        ax = f.add_subplot(332)
         ax.hist(tree['H_cal_etotnorm'],bins=c.setbin(tree['H_cal_etotnorm'],200,0,2.0),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
         ax.hist(c.add_cut(tree['H_cal_etotnorm'],"h_cal_nt"),bins=c.setbin(tree['H_cal_etotnorm'],200,0,2.0),label='cut',histtype='step', alpha=0.5, stacked=True, fill=True)
         plt.yscale('log')
         plt.xlabel('H_cal_etotnorm')
         plt.ylabel('Count')
 
-        ax = f.add_subplot(232)
+        ax = f.add_subplot(333)
         ax.hist(tree['H_cer_npeSum'],bins=c.setbin(tree['H_cer_npeSum'],200,0,60),label='no cut',histtype='step', alpha=0.5,stacked=True, fill=True)
         ax.hist(c.add_cut(tree['H_cer_npeSum'],"h_cer_nt"),bins=c.setbin(tree['H_cer_npeSum'],200,0,60),label='cut',histtype='step', alpha=0.5, stacked=True, fill=True)
         plt.yscale('log')
@@ -314,21 +321,21 @@ def pid_cuts():
         plt.ylabel('Count')
 
     if (not SHMS_PS == None) or (not COIN_PS == None):
-        ax = f.add_subplot(233)
+        ax = f.add_subplot(334)
         ax.hist(tree['P_cal_etotnorm'],bins=c.setbin(tree['P_cal_etotnorm'],200,0,4),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
         ax.hist(c.add_cut(tree['P_cal_etotnorm'],"p_cal_nt"),bins=c.setbin(tree['P_cal_etotnorm'],200,0,4),label='cut',histtype='step', alpha=0.5, stacked=True, fill=True)
         plt.yscale('log')
         plt.xlabel('P_cal_etotnorm')
         plt.ylabel('Count')
 
-        ax = f.add_subplot(234)
+        ax = f.add_subplot(335)
         ax.hist(tree['P_hgcer_npeSum'],bins=c.setbin(tree['P_hgcer_npeSum'],200,0,200),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
         ax.hist(c.add_cut(tree['P_hgcer_npeSum'],"p_hgcer_nt"),bins=c.setbin(tree['P_hgcer_npeSum'],200,0,200),label='cut',histtype='step', alpha=0.5, stacked=True, fill=True)
         plt.yscale('log')
         plt.xlabel('P_hgcer_npeSum')
         plt.ylabel('Count')
 
-        ax = f.add_subplot(235)
+        ax = f.add_subplot(336)
         ax.hist(tree['P_aero_npeSum'],bins=c.setbin(tree['P_aero_npeSum'],200,0,400),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
         ax.hist(c.add_cut(tree['P_aero_npeSum'],"p_aero_nt"),bins=c.setbin(tree['P_aero_npeSum'],200,0,400),label='cut',histtype='step', alpha=0.5, stacked=True, fill=True)
         plt.yscale('log')
@@ -336,7 +343,7 @@ def pid_cuts():
         plt.ylabel('Count')   
     
         if ANATYPE == "Pion":
-            ax = f.add_subplot(236)
+            ax = f.add_subplot(337)
             ax.hist(tree['P_ngcer_npeSum'],bins=c.setbin(tree['P_ngcer_npeSum'],200,0,250),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
             ax.hist(c.add_cut(tree['P_ngcer_npeSum'],"p_ngcer_nt"), bins=c.setbin(tree['P_ngcer_npeSum'],200,0,250),label='cut',histtype='step', alpha=0.5, stacked=True, fill=True)
             plt.yscale('log')
