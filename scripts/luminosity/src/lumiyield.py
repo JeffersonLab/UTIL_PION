08939f434be7dc5b201a63a331d71865708e478b
heinricn <heinricn@uregina.ca>
2024-09-18 12:04:44 -0600

    fixed bug in plotting script

--- scripts/luminosity/src/lumiyield.py
+++ scripts/luminosity/src/lumiyield.py
@@ -404,7 +404,7 @@ def pid_cuts():
     
     if (not COIN_PS == None):
         ax = f.add_subplot(338)
-        ax.hist(P_ngcer_npeSum,bins=c.setbin(CTime_ePiCoinTime_ROC1,600,-250,250),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
+        ax.hist(c.add_cut(CTime_ePiCoinTime_ROC1,"h_ecut_lumi_nt"),bins=c.setbin(CTime_ePiCoinTime_ROC1,600,-250,250),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
         ax.hist(c.add_cut(CTime_ePiCoinTime_ROC1,"c_epi_nt_lumi"), bins=c.setbin(CTime_ePiCoinTime_ROC1,600,-250,250),label='cut',histtype='step', alpha=0.5, stacked=True, fill=True)
         plt.yscale('log')
         plt.xlabel('CTime_ePiCoinTime_ROC1 + No Track Cut')
@@ -412,7 +412,7 @@ def pid_cuts():
 #            c_noTrack = c.add_cut(tree["CTime_ePiCoinTime_ROC1"], "c_epi_nt_lumi")
         
         ax = f.add_subplot(339)
-        ax.hist(P_ngcer_npeSum,bins=c.setbin(CTime_ePiCoinTime_ROC1,100,-10,10),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
+        ax.hist(c.add_cut(CTime_ePiCoinTime_ROC1,"h_ecut_lumi"),bins=c.setbin(CTime_ePiCoinTime_ROC1,100,-10,10),label='no cut',histtype='step',alpha=0.5, stacked=True, fill=True)
         ax.hist(c.add_cut(CTime_ePiCoinTime_ROC1,"c_epitrack_lumi_after"), bins=c.setbin(CTime_ePiCoinTime_ROC1,100,-10,10),label='cut',histtype='step', alpha=0.5, stacked=True, fill=True)
         plt.yscale('log')
         plt.xlabel('CTime_ePiCoinTime_ROC1 + Track Cut')
