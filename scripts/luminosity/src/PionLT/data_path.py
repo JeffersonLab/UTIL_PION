731ca687cced0bde5c66e60cb9e789c4b938a76f
heinricn <heinricn@uregina.ca>
2024-10-10 11:13:02 -0600

    added exclusive lumi data

--- scripts/luminosity/src/PionLT/data_path.py
+++ scripts/luminosity/src/PionLT/data_path.py
@@ -95,6 +95,32 @@ def get_file(inp_name,SCRIPTPATH):
             inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/sidis/lumi_data_sidis2.csv"
             out_f = SCRIPTPATH+"/luminosity/OUTPUTS/sidis/yield_data_sidis2.csv"
             print("\nGrabbing input...\n\n%s" % str(inp_f))
+    elif "EXCLUSIVE" in inp_name.upper():
+        if "1" in inp_name.upper():
+            target = "LH2"
+            inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/lumi_data_exc1.csv"
+            out_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/yield_data_exc1.csv"
+            print("\nGrabbing input...\n\n%s" % str(inp_f))
+        if "2" in inp_name.upper():
+            target = "LH2"
+            inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/lumi_data_exc2.csv"
+            out_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/yield_data_exc2.csv"
+            print("\nGrabbing input...\n\n%s" % str(inp_f))
+        if "3" in inp_name.upper():
+            target = "LH2"
+            inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/lumi_data_exc3.csv"
+            out_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/yield_data_exc3.csv"
+            print("\nGrabbing input...\n\n%s" % str(inp_f))
+        if "4" in inp_name.upper():
+            target = "LH2"
+            inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/lumi_data_exc4.csv"
+            out_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/yield_data_exc4.csv"
+            print("\nGrabbing input...\n\n%s" % str(inp_f))
+        if "5" in inp_name.upper():
+            target = "LH2"
+            inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/lumi_data_exc5.csv"
+            out_f = SCRIPTPATH+"/luminosity/OUTPUTS/ExclusiveLumi/yield_data_exc5.csv"
+            print("\nGrabbing input...\n\n%s" % str(inp_f))
     else:
         target = "carbon"
         inp_f = SCRIPTPATH+"/luminosity/OUTPUTS/lumi_data.csv"
