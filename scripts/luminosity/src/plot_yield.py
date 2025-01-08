d32abc0271e667b0bd913a792dac54eb6c10c168
heinricn <heinricn@uregina.ca>
2025-01-08 14:26:00 -0600

    fixed syntax issue

--- scripts/luminosity/src/plot_yield.py
+++ scripts/luminosity/src/plot_yield.py
@@ -175,8 +175,8 @@ def calc_yield():
     boilingCorr = 1 - (yield_dict[current]*makeList("curr_corr")*(2.80/10000)) #boiling correction for 2.8%/100uA slope
     uncer_boilingCorr = np.sqrt((((uncern_slope**2)+ (uncern_intercept**2/yield_dict[current]))/slope**2) + (0.17**2+0.69**2)/(2.8**2))
 
-    yield_dict.update({"boilingCorr"}: boilingCorr)
-    yield_dict.update({"uncer_boilingCorr"}: uncer_boilingCorr)
+    yield_dict.update({"boilingCorr": boilingCorr})
+    yield_dict.update({"uncer_boilingCorr": uncer_boilingCorr})
 
     gwidth = 55*(10**(-9)) #gate width == 55 nanometers
         
