a342dffad2319bdd19da9ce9e253f1731252ebcc
heinricn <heinricn@uregina.ca>
2025-01-16 15:03:30 -0600

    bug fix

--- scripts/luminosity/src/plot_yield.py
+++ scripts/luminosity/src/plot_yield.py
@@ -179,6 +179,7 @@ def calc_yield():
 
     rateSlope = 2.9/(10*8) #adhoc rate correction for 2.9%/MHz slope
     rateSlopeUncer = 0.1/(10*8)  #statistical uncertainty
+    print("rate slope, rateSlopeUncer\n")
     print(rateSlope, rateSlopeUncer)
     rateHMSCorr = 1 - (yield_dict["rate_HMS3-4"]*(rateSlope))         
     uncer_rateHMSCorr = (yield_dict["rate_HMS3-4"]*rateSlopeUncer)**2 + (rateSlope*yield_dict["uncern_HMS3-4_evts_scaler"])**2
